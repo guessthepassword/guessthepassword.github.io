bd2758fe7dceb5b3d46c117ec9fea45deb52e550
guessthepassword <167563281+guessthepassword@users.noreply.github.com>
2024-04-19 18:45:24 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    let x = document.getElementById("numb").value;
+    if (x == "balls") {
+      window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+      return false;
+    }
+  }
